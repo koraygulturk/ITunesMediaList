cdaf2ec55696263f966526a0ac1313424ea2871d
koray gültürk <koraygulturk@gmail.com>
2019-02-07 23:45:24 +0300

    Initial Commit

---
+++ ITunesMediaList/Extensions/UIColor+Theme.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Theme.swift
+//  ITunesMediaList
+//
+//  Created by koray gulturk on 7.02.2019.
+//  Copyright © 2019 kogo. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var cellTitle = UIColor(displayP3Red: 91/255, green: 91/255, blue: 91/255, alpha: 1)
+}
---
+++ ITunesMediaList/Services/MediaService.swift
@@ -0,0 +1,21 @@
+//
+//  MediaService.swift
+//  ITunesMediaList
+//
+//  Created by koray gulturk on 7.02.2019.
+//  Copyright © 2019 kogo. All rights reserved.
+//
+
+import UIKit
+
+enum ServiceError: String, Error {
+    case networkError = "Bağlantı Hatası"
+}
+
+class MediaService: MediaServiceProtocol  {
+
+}
+
+protocol MediaServiceProtocol {
+    
+}
